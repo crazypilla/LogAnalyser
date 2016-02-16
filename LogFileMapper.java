03b4d0d70872680ee39cd11a0e1987ba5af077bc
Harshita Vallurupalli <hershey2703@gmail.com>
2016-02-15 19:21:57 -0500

    Create LogFileMapper.java

---
+++ LogFileMapper.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.Mapper;
+
+
+/* Mapper implementation : reading the input and */
+
+public class LogFileMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+
+	@Override
+	public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+			throws IOException {
+		String[] fields = value.toString().split(" ");
+		if (fields.length > 3) {
+			String ip = fields[0];
+			String[] dtFields = fields[3].split("/");
+			if (dtFields.length > 1) {
+				String theMonth = dtFields[1];
+
+				output.collect(new Text(ip), new Text(theMonth));
+			}
+		}
+	}
+}
